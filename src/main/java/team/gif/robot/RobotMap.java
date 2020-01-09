0244d9f4cbb71b634300cd787d089ab79a52f021
fuzzyfish1 <zain@accucalc.com>
2020-01-08 20:09:40 -0600

    setspeed

--- src/main/java/team/gif/robot/RobotMap.java
+++ src/main/java/team/gif/robot/RobotMap.java
@@ -1,4 +1,15 @@
 package team.gif.robot;
 
 public class RobotMap {
+
+    //drivetrain motors based on Spark Maxs
+    public static int rightfrontneo = 0;
+    public int rightbackneo =0;
+    public int leftfrontneo =0;
+    public int leftbackneo =0;
+
+
+
+
+
 }
--- src/main/java/team/gif/robot/subsystems/drivetrain.java
+++ src/main/java/team/gif/robot/subsystems/drivetrain.java
@@ -1,4 +1,35 @@
 package team.gif.robot.subsystems;
 
-public class drivetrain {
+import edu.wpi.first.wpilibj.PWMSparkMax;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import team.gif.robot.RobotMap;
+
+public class drivetrain implements Subsystem {
+
+
+    // PWM pulse width modulator
+    PWMSparkMax rightfrontneo = new PWMSparkMax(RobotMap.rightfrontneo);
+    PWMSparkMax rightbackneo = new PWMSparkMax(RobotMap.rightfrontneo);
+    PWMSparkMax leftfrontneo = new PWMSparkMax(RobotMap.rightfrontneo);
+    PWMSparkMax leftbackneo = new PWMSparkMax(RobotMap.rightfrontneo);
+
+    public drivetrain(){
+
+
+
+
+    }
+
+    public void setspeed(double leftspeed, double rightspeed){
+
+            //run by percent default always for PWM
+            leftbackneo.set(leftspeed);
+            leftfrontneo.set(leftspeed);
+            rightfrontneo.set(rightspeed);
+            rightbackneo.set(rightspeed);
+
+    }
+
+
+
 }
