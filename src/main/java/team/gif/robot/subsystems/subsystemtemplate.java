4e073e6e42c805a3b27eb62c921f9b1c4397dd0f
fuzzyfish1 <zain@accucalc.com>
2020-01-09 19:29:58 -0600

    yo this commit the subsystem template about 1/8 done

---
+++ src/main/java/team/gif/robot/subsystems/subsystemtemplate.java
@@ -0,0 +1,17 @@
+package team.gif.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import team.gif.robot.RobotMap;
+
+public class subsystemtemplate implements Subsystem {
+
+    /*motors
+        PWM pulse width modulator
+        PWMSparkMax rightfrontneo = new PWMSparkMax(RobotMap.rightfrontneo);
+    */
+    public subsystemtemplate(){}
+
+
+
+
+}
