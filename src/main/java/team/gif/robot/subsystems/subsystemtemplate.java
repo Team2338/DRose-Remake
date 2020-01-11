49dfabd6031b39fb49054738f0c8ee71c76db24e
fuzzyfish1 <zain@accucalc.com>
2020-01-10 19:43:01 -0600

    subsystem template added, fixed my sparks libraary

--- src/main/java/team/gif/robot/subsystems/subsystemtemplate.java
+++ src/main/java/team/gif/robot/subsystems/subsystemtemplate.java
@@ -1,26 +1,24 @@
 package team.gif.robot.subsystems;
 
+import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import team.gif.robot.RobotMap;
-//import ctre.
+import com.ctre.phoenix.motorcontrol.can.TalonSRX; //talon import
+import com.ctre.phoenix.motorcontrol.can.VictorSPX; //victor import
+import com.revrobotics.CANSparkMax;//CANSparkMax
 public class subsystemtemplate implements Subsystem {
-
     /*
         PWMSparkMax
         PWMSparkMax rightfrontneo = new PWMSparkMax(RobotMap.rightfrontneo);
     */
     /*
         Victors
-        Victor = new Victor​(RobotMap.rightfrontneo);
+        Victor v = new Victor​(RobotMap.rightfrontneo);
     */
     /*
         Talons
-
-
-
-
+        Talon talon = new Talon(RobotMap.talon);
     */
-
     public subsystemtemplate(){}
 
 
